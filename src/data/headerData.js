b35af2a1081a7df99c9409922e7d742b915f0d37
chesspamungkas <chesspamungkas@gmail.com>
2023-01-16 02:36:15 +0700

    Updates

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,6 +5,6 @@ export const headerData = {
     title: "Senior Web Developer",
     desciption1:"I have worked as a Web Developer for the past 14 years. I have always been passionate developing user-friendly websites. I have all the skills to create a new high-quality website or improve existing websites for individuals and organizations, using the latest techniques and trends.",
     desciption2:"I have extensive knowledge of PHP, Laravel, CodeIgniter, WordPress, CSS, SCSS, Bootstrap, JavaScript, TypeScript, Node.js, Angular, React, Phyton, Golang, REST API, jQuery, AJAX, JSON, AWS, etc.",
-    image: 'https://media.licdn.com/dms/image/D5603AQFapXcUXNKffA/profile-displayphoto-shrink_800_800/0/1670768420352?e=1678924800&v=beta&t=AxHmh2qFWKw8CONdFCpwwaVs5gcQazRjQ3x53nyeFp8',
+    image: 'https://chesspamungkas.com/src/assets/png/Catur-Pamungkas-LinkedIn-Photo-Crop.png',
     resumePdf: resume
 }
